0059b129c71ee54249eb378685b2edb10a330d0c
madinarsp <madinarsp20@gmail.com>
2021-09-21 08:09:35 +0600

    fix: fix hamburger menu behavior

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const hamburgerBtn = document.querySelector('.hamburger-menu');
 const navList = document.querySelector('.nav_list');
 const body = document.querySelector('body');
-const navSection = document.querySelector('.nav');
+const navItems = document.querySelectorAll('.nav-item');
+console.log(navItems);
+
 
 hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.classList.toggle('open');
     navList.classList.toggle('open');
     body.classList.toggle('locked');
 });
+
+navItems.forEach(item => addEventListener('click', (event) => {
+    if(!event.target.classList.contains('hamburger-menu')) {
+        hamburgerBtn.classList.toggle('open');
+        navList.classList.toggle('open');
+        body.classList.toggle('locked');
+    }
+    
+}));
